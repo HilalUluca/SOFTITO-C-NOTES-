fefcc0090fed4302bd040eda2c994eb90066c4e3
antagnst <125604608+HilalUluca@users.noreply.github.com>
2025-08-16 19:17:59 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Burada otomatik metod oluştu(neye göre tek çift kontrol etsin textbox1)
+
+            int sayi = Convert.ToInt32(textBox1.Text);  //daha önce konsoldan aldığım için konsol diyordum
+            if (sayi % 2 == 0)
+            {
+                MessageBox.Show($"{sayi}çift");
+            }
+            else
+            {
+                MessageBox.Show($"{sayi}tek");
+
+            }
+        }
+    }
+}
